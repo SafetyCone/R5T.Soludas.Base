2b76433bc99e84891306eb6b0f766a3d83daab95
MinexAutomation <minex.automation@gmail.com>
2020-01-30 23:04:44 -0800

    Added IVisualStudioSolutionFileValueEqualityComparer service definition interface type.

---
+++ source/R5T.Soludas.Base/Code/Services/Definitions/IVisualStudioSolutionFileValueEqualityComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using R5T.Cambridge.Types;
+using R5T.Vandalia;
+
+
+namespace R5T.Soludas
+{
+    public interface IVisualStudioSolutionFileValueEqualityComparer : IValueEqualityComparer<SolutionFile>
+    {
+        
+    }
+}
